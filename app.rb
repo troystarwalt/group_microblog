1aa669d1ce23dbb52f30bf3168745746613d61ed
Troy Starwalt <troy.starwalt@gmail.com>
2015-10-13 17:34:15 -0400

    Changes to Sign In

--- app.rb
+++ app.rb
@@ -13,12 +13,23 @@ set :bind, "0.0.0.0"
 set :sessions, true
 
 get '/' do 
-	@users = User.all
+	@user = User.all
     erb :index
 end
 
 get '/signup' do
 	erb :signup
+end
+
+get '/login' do
+	flash[:notice] = "Welcome!"
+	erb :login
+end
+
+get '/logout' do
+	flash[:notice] = "You'll Be Back."
+	session.clear
+	redirect to("index")
 end
 
 post "/user_create" do
@@ -34,10 +45,6 @@ post "/user_create" do
 		})
 		redirect to("/user_create_success")
 	end
-end
-
-get '/login' do
-	erb :login
 end
 
 post "/user_login_attempt" do
@@ -61,19 +68,26 @@ post "/user_login_attempt" do
 	end
 end
 
-
+get "/ourtweet" do
+	erb :ourtweet
+end
 
 get "/user_create_error" do
 	"Please try again."
 end
 
 get "/user_create_success" do
+	@user_current = User.last
 	@users = User.all
-	erb :user_create_success
-	
+	session[:user_id] = @user.id
+	erb :user_create_success	
 end
 
 get "/profile" do
-	@users = User.find(session[:user.id])
+	@user = User.find(session[:user_id])
 	erb :profile
+end
+
+get "/login_error" do
+	"Sorry Your username or password was not correct."
 end
