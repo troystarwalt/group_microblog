9e7d75aa5cc302e2ba8d97070096552a863870c1
Troy Starwalt <troy.starwalt@gmail.com>
2015-10-14 09:56:14 -0400

    Added Throw Table

--- db/migrate/20151013215658_add_throws_table.rb
+++ db/migrate/20151013215658_add_throws_table.rb
@@ -3,7 +3,7 @@ class AddThrowsTable < ActiveRecord::Migration
   	create_table :throws do |t|
 	  	t.integer 		:user_id		
 		t.text 			:throwpost
-		t.datetime 		:create_at
+		t.datetime 		:created_at
   	end
   end
 end
