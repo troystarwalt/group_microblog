8d490722daf04c4f3fa31fe449103f5efa31f2d2
Troy Starwalt <troy.starwalt@gmail.com>
2015-10-14 22:29:20 -0400

    Added some styles and email protect

--- models.rb
+++ models.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :throws
+		def isSecurePassword
+		!self.password.nil? && 
+		self.password.length > 8 &&
+		!self.password.match("[0-9]").nil? &&
+		!self.password.match("[A-Z]").nil?
+	end
 end
 
 class Throw < ActiveRecord::Base
